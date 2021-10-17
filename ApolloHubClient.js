3c0ae40b704d4a07d9088ec92cb65ba814c00d4f
Gav <86022295+GanOnsauce@users.noreply.github.com>
2021-10-17 10:47:22 -0500

    JavaScript Client
    LESS GOO

---
+++ ApolloHubClient.js
@@ -0,0 +1,36 @@
+<title> ApolloHub Client </title>
+ApolloHub Client
+<br>
+To Find Your Game Use CTRL + F
+<br>
+ <button onclick="window.location.href='clientfiles/ballc.html';">
+      Ball Collector
+    </button>
+  <br>
+    <button onclick="window.location.href='clientfiles/ETS.html';">
+      Escaping The Spaceship // Exiting The Spaceship
+    </button>
+    <br>
+     <button onclick="window.location.href='clientfiles/GTVe.html';">
+      Glide To Vertterre
+    </button>
+  <br>
+  <button onclick="window.location.href='clientfiles/SSt.html';">
+      Speedrun Stickman
+    </button>
+    <br>
+     <button onclick="window.location.href='clientfiles/ShooterOS.html';">
+     Shooter OS (Beta & Not Beta)
+    </button>
+    <br>
+      <button onclick="window.location.href='clientfiles/ThxDoggo.html';">
+      Thx Doggo
+    </button>
+  <br>
+<button onclick="window.location.href='clientfiles/dh.html';">
+      Duck Hunter
+    </button>
+<br>
+<button onclick="window.location.href='clientfiles/';">
+      geometrical dungeon
+    </button>
